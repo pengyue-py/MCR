d721488cb05525851218cab665258ae29eb71db5
pengyue-py <56428687+pengyue-py@users.noreply.github.com>
2024-09-16 19:35:15 +0800

    Update MCR.cpp
    add some comments

--- MCR.cpp
+++ MCR.cpp
@@ -35,7 +35,7 @@ false to true.
  cout << "Player 1: ";
  else
  cout << "Player 2: ";
- cout << "Which cell to mark? i:[1..3], j:[1..3]: ";
+ cout << "Which cell to mark? i:[1..3], j:[1..3]: "; // user alerts
  cin >> i >> j;
  if (turn == false)
  game[i][j] = 'X';
